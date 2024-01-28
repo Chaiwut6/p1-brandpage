e821737e00dd61a45abdec77dafb276a7ee2610e
Chaiwut <chivwut0646@gmail.com>
2024-01-28 17:55:42 +0700

    first commit test react

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import Navbar from './components/Navbar'
+import Content from './components/content'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+    <Navbar/>
+    <Content/>
+    </>
+  )
+}
+
+export default App
